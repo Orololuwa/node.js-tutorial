504fc61a40229ec0d81a65fa98ce2cb3141df4c8
unknown <ea@afarapartners.com>
2022-02-14 12:19:13 +0100

    routes refactored

--- app.js
+++ app.js
@@ -8,15 +8,32 @@ const tours = JSON.parse(
 const app = express();
 app.use(express.json());
 
-app.get('/api/v1/tours', (req, res) => {
+const getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
     results: tours.length,
     tours,
   });
-});
+};
 
-app.post('/api/v1/tours', (req, res) => {
+const getTour = (req, res) => {
+  const id = +req.params.id;
+  const tour = tours.find((el) => el.id === id);
+
+  if (!tour) {
+    res.status(404).json({
+      status: 'failed',
+      message: 'invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    tour,
+  });
+};
+
+const createTour = (req, res) => {
   const id = tours[tours.length - 1].id + 1;
   const newTour = {
     id,
@@ -34,7 +51,57 @@ app.post('/api/v1/tours', (req, res) => {
       });
     }
   );
-});
+};
+
+const updateTour = (req, res) => {
+  const id = +req.params.id;
+  const tour = tours.find((el) => el.id === id);
+
+  if (!tour) {
+    res.status(404).json({
+      status: 'failed',
+      message: 'invalid ID',
+    });
+  }
+
+  res.status(201).json({
+    status: 'success',
+    tour: {
+      ...tour,
+      ...req.body,
+    },
+  });
+};
+
+const deleteTour = (req, res) => {
+  const id = +req.params.id;
+  const tour = tours.find((el) => el.id === id);
+
+  if (!tour) {
+    res.status(404).json({
+      status: 'failed',
+      message: 'invalid ID',
+    });
+  }
+
+  res.status(204).json({
+    status: 'success',
+    tour: null,
+  });
+};
+
+// app.get('/api/v1/tours', getAllTours);
+// app.get('/api/v1/tours/:id', getTour);
+// app.post('/api/v1/tours', createTour);
+// app.patch('/api/v1/tours/:id', updateTour);
+// app.delete('/api/v1/tours/:id', deleteTour);
+
+app.route('/api/v1/tours').get(getAllTours).post(createTour);
+app
+  .route('/api/v1/tours/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
 
 const port = 7000;
 app.listen(port, () => {

8f7c9da197eed93c41012f2595791ad4d2152f8c
unknown <ea@afarapartners.com>
2022-02-23 11:07:00 +0100

    mongoose starter

--- server.js
+++ server.js
@@ -1,7 +1,52 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
+
+const DB = process.env.DATABASE_2.replace(
+  '<PASSWORD>',
+  process.env.DB_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('DB connected Succesfully');
+  });
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Camp Scammer',
+  rating: 4.8,
+  price: 375,
+});
+
+testTour
+  .save()
+  .then((doc) => console.log(doc))
+  .catch((err) => console.log('Error 🧯:', err));
 
 const port = process.env.NODE_DOCKER_PORT;
 app.listen(port, () => {

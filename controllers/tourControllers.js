f86b0028310e4db84f0b6b75fc82808e8ce5566d
Orololuwa <67964734+Orololuwa@users.noreply.github.com>
2022-03-04 14:08:47 +0100

    pagination

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -33,11 +33,27 @@ exports.getAllTours = async (req, res) => {
       query = query.select('-__v');
     }
 
+    //PAGINATION
+    const page = +req.query.page || 1;
+    const limit = +req.query.limit || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip > numTours) {
+        throw new Error('Page not found');
+      }
+    }
+
     const tours = await query;
 
     res.status(200).json({
       status: 'success',
       results: tours.length,
+      page: page,
+      pageSize: limit,
       data: {
         tours,
       },

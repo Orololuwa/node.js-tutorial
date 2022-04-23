d301a53c9212069ed98e7cdc258cfec571b6ca17
Orololuwa <ea@afarapartners.com>
2022-04-23 23:20:35 +0100

    error handling: development vs production

--- controllers/errorControllers.js
+++ controllers/errorControllers.js
@@ -1,9 +1,39 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    error: err,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  // Operational trusted Error: Send message to the client
+  if ((err, isOperational)) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+    //   Programming or other unknownn errors: don't leak details to the client
+  } else {
+    //   1) Log the error
+    console.error('Error ', err);
+
+    // 2) S end generic message
+    res.status(500).json({
+      status: 'fail',
+      message: 'Oops! Something went wrong',
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  return res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    return sendErrorDev(err, res);
+  } else if ((process.env.NODE_ENV = 'production')) {
+    return sendErrorProd(err, res);
+  }
 };
